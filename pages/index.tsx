aad7651cba985f99ef30b9e4431d9974276493d3
Eugene Denisov <jackyapa6eu@yandex.ru>
2023-04-04 23:17:20 +0600

    qr scanner

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,17 +1,56 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import Card from "../src/components/Card";
-import MyDocument from './_document';
+import { QrReader}  from 'react-qr-reader';
+import "webrtc-adapter/out/adapter.js";
 
-const HomePage = ({message}) => {
+const HomePage = ({message = 'xxx'}) => {
+    const [result, setResult] = useState('');
+    const handleScan = (data) => {
+        if (data) {
+            setResult(data.text);
+            alert(data.text)
+        }
+    };
+
+    const handleError = (err) => {
+        alert(err);
+    };
+
+    const handleResult = (result) => {
+        console.log(result); // здесь можно выполнить необходимые действия с результатом сканирования
+    };
+
+    useEffect(() => {
+        navigator.mediaDevices.getUserMedia({ video: true, audio: false })
+            .then(function(stream) {
+                console.log('THEN')
+            })
+            .catch(function(err) {
+                alert("An error occurred: " + err);
+            });
+    }, [])
+
+
+
     return (
         <div>
-            <img src="https://mc.yandex.ru/watch/92850603" style="position:absolute; left:-9999px;" alt="" />
-            <h1>Hello, world! message: </h1>
+            <img src="https://mc.yandex.ru/watch/92850603" style={{position: 'absolute', left: '-9999px'}} alt="" />
+            <h1>Hello, world! message: {result}</h1>
+            <div style={{width: '50%', background: 'red'}}>
+                <QrReader
+                    delay={800}
+                    onError={handleError}
+                    onScan={handleScan}
+                    style={{ width: '100%' }}
+                    onResult={handleResult}
+                />
+            </div>
             <Card msg={message}/>
         </div>
     );
 };
 
+/*
 export async function getServerSideProps() {
     return {
         props: {
@@ -19,5 +58,5 @@ export async function getServerSideProps() {
         }
     };
 }
-
+ */
 export default HomePage;
