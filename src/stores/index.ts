bbb81acaabb89f52620ec094e355ef241400b82a
Eugene Denisov <jackyapa6eu@yandex.ru>
2024-07-13 18:57:44 +0300

    Initial commit with local project

---
+++ src/stores/index.ts
@@ -0,0 +1,20 @@
+import { makeAutoObservable } from 'mobx';
+
+class AppStore {
+  count = 0;
+
+  constructor() {
+    makeAutoObservable(this);
+  }
+
+  increment = () => {
+    this.count += 1;
+  }
+
+  decrement = () => {
+    this.count -= 1;
+  }
+}
+
+const appStore = new AppStore();
+export default appStore;
