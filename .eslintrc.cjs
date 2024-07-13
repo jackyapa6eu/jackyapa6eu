09ae05d7b03c9533db8e5237290a789eaf5c6139
Eugene Denisov <jackyapa6eu@yandex.ru>
2024-07-13 19:24:29 +0300

    eslint prettier added

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,18 +1,31 @@
 module.exports = {
-  root: true,
-  env: { browser: true, es2020: true },
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: ['./tsconfig.json', './tsconfig.node.json'],
+    tsconfigRootDir: __dirname,
+  },
   extends: [
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
-    'plugin:react-hooks/recommended',
+    'plugin:@typescript-eslint/recommended-type-checked',
+    'plugin:@typescript-eslint/strict-type-checked',
+    'plugin:react/recommended',
+    'plugin:react/jsx-runtime',
+    'plugin:prettier/recommended',
   ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
-  parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
+  plugins: [
+    '@typescript-eslint',
+    'react',
+    'prettier',
+  ],
   rules: {
-    'react-refresh/only-export-components': [
-      'warn',
-      { allowConstantExport: true },
-    ],
+    'prettier/prettier': 'error',
   },
-}
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+};
--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,27 @@
 // import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import {observer} from "mobx-react-lite";
-import appStore from "./stores";
+import reactLogo from './assets/react.svg';
+import viteLogo from '/vite.svg';
+import './App.css';
+import { observer } from 'mobx-react-lite';
+import appStore from './stores';
 
-const App = observer( () => {
+const App = observer(() => {
   // const [count, setCount] = useState(0)
-  const {count, increment} = appStore;
+  const { count, increment } = appStore;
 
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
+        <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
-        <a href="https://react.dev" target="_blank">
+        <a href="https://react.dev" target="_blank" rel="noreferrer">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={increment}>
-          count is {count}
-        </button>
+        <button onClick={increment}>count is {count}</button>
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
@@ -32,7 +30,7 @@ const App = observer( () => {
         Click on the Vite and React logos to learn more
       </p>
     </>
-  )
-})
+  );
+});
 
-export default App
+export default App;
